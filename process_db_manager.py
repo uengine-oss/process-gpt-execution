6ff628a85cde92ea7af478a9c61fdea64a4c7c96
ohsy818 <ohsy818@uengine.org>
2025-08-29 12:12:46 +0900

    rollback vecs api

--- process_db_manager.py
+++ process_db_manager.py
@@ -1,6 +1,5 @@
 from fastapi import HTTPException, Request
-from database import update_user_admin, create_user, invite_user, set_initial_info, get_vecs_memories, delete_vecs_memories
-
+from database import update_user_admin, create_user, invite_user, set_initial_info
 
 async def combine_input_with_tenant_id(request: Request):
     json_data = await request.json()
@@ -27,42 +26,12 @@ async def combine_input_with_user_info(request: Request):
     input = json_data.get('input')
     return update_user_admin(input)
 
-async def get_vecs_documents(request: Request):
-    input = await request.json()
-    agent_id = input.get('agent_id')
-    limit = input.get('limit', 10)
-    
-    if not agent_id:
-        raise HTTPException(status_code=400, detail="agent_id is required")
-    if not limit:
-        raise HTTPException(status_code=400, detail="limit is required")
-    if limit < 1:
-        raise HTTPException(status_code=400, detail="limit must be greater than 0")
-    if limit > 100:
-        limit = 100
-
-    return get_vecs_memories(agent_id, limit)
-
-async def delete_vecs_documents(request: Request):
-    input = await request.json()
-    agent_id = input.get('agent_id')
-    memory_id = input.get('memory_id')
-    if not agent_id:
-        raise HTTPException(status_code=400, detail="agent_id is required")
-    if not memory_id:
-        raise HTTPException(status_code=400, detail="memory_id is required")
-
-    return delete_vecs_memories(agent_id, memory_id)
-
-
 def add_routes_to_app(app) :
     app.add_api_route("/set-tenant", combine_input_with_tenant_id, methods=["POST"])
     app.add_api_route("/create-user", combine_input_with_new_user_info, methods=["POST"])
     app.add_api_route("/invite-user", combine_input_with_invite_user_info, methods=["POST"])
     app.add_api_route("/set-initial-info", combine_input_with_set_initial_info, methods=["POST"])
     app.add_api_route("/update-user", combine_input_with_user_info, methods=["POST"])
-    app.add_api_route("/get-vecs-documents", get_vecs_documents, methods=["POST"])
-    app.add_api_route("/delete-vecs-documents", delete_vecs_documents, methods=["POST"])
 
 
 """
